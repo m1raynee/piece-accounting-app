4c94297929a69b64b06799dfd06d3908f3074485
m1raynee <71838742+m1raynee@users.noreply.github.com>
2026-09-19 04:50:14 +0300

    Spring Initial commit

---
+++ src/main/java/com/m1raynee/pieceaccountingapp/PieceaccountingappApplication.java
@@ -0,0 +1,13 @@
+package com.m1raynee.pieceaccountingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PieceaccountingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PieceaccountingappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/m1raynee/pieceaccountingapp/PieceaccountingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m1raynee.pieceaccountingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PieceaccountingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
